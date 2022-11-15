48dd521be0cb2eecf310fb3cc76e244d532db980
eu <eu@DESKTOP-0E51COI>
2022-11-15 12:33:51 -0300

    Interface

---
+++ src/Interface/Tela.java
@@ -0,0 +1,384 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package Interface;
+
+public class Tela extends javax.swing.JFrame {
+
+    public Tela() {
+        initComponents();
+        TelaDocente.setVisible(false);
+        TelaEstudante.setVisible(false);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        TelaLogin = new javax.swing.JPanel();
+        loginUsuario = new javax.swing.JTextField();
+        loginSenha = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
+        Título = new javax.swing.JLabel();
+        Imagem = new javax.swing.JLabel();
+        TelaDocente = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jDesktopPane1 = new javax.swing.JDesktopPane();
+        BotaoSairDocente = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        TelaEstudante = new javax.swing.JPanel();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        TelaLogin.setBackground(new java.awt.Color(255, 255, 255));
+        TelaLogin.setMinimumSize(new java.awt.Dimension(800, 600));
+        TelaLogin.setOpaque(false);
+        TelaLogin.setPreferredSize(new java.awt.Dimension(800, 600));
+        TelaLogin.setLayout(null);
+
+        loginUsuario.setFont(new java.awt.Font("Gadugi", 2, 12)); // NOI18N
+        loginUsuario.setForeground(new java.awt.Color(204, 204, 204));
+        loginUsuario.setText("Usúario");
+        loginUsuario.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ClickMouseUsuario(evt);
+            }
+        });
+        TelaLogin.add(loginUsuario);
+        loginUsuario.setBounds(280, 244, 240, 30);
+
+        loginSenha.setFont(new java.awt.Font("Gadugi", 2, 12)); // NOI18N
+        loginSenha.setForeground(new java.awt.Color(204, 204, 204));
+        loginSenha.setText("Senha");
+        loginSenha.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ClickMouseSenha(evt);
+            }
+        });
+        loginSenha.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                loginSenhaKeyPressed(evt);
+            }
+        });
+        TelaLogin.add(loginSenha);
+        loginSenha.setBounds(280, 290, 240, 30);
+
+        jButton2.setText("Sair");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotaoSairTelaLogin(evt);
+            }
+        });
+        TelaLogin.add(jButton2);
+        jButton2.setBounds(360, 390, 72, 23);
+
+        jButton1.setBackground(new java.awt.Color(255, 255, 102));
+        jButton1.setFont(new java.awt.Font("Malgun Gothic", 0, 12)); // NOI18N
+        jButton1.setText("Acessar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        TelaLogin.add(jButton1);
+        jButton1.setBounds(360, 350, 72, 24);
+
+        Título.setFont(new java.awt.Font("Algerian", 0, 65)); // NOI18N
+        Título.setText("LOGIN");
+        TelaLogin.add(Título);
+        Título.setBounds(310, 140, 180, 60);
+
+        Imagem.setFont(new java.awt.Font("Gadugi", 2, 12)); // NOI18N
+        Imagem.setForeground(new java.awt.Color(204, 204, 204));
+        Imagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagem/wallpapersden.com_landscape-pixel-art_800x600.jpg"))); // NOI18N
+        Imagem.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                ImagemMouseClicked(evt);
+            }
+        });
+        TelaLogin.add(Imagem);
+        Imagem.setBounds(0, 0, 800, 600);
+
+        TelaDocente.setMinimumSize(new java.awt.Dimension(800, 600));
+        TelaDocente.setOpaque(false);
+        TelaDocente.setLayout(null);
+
+        jPanel1.setBackground(new java.awt.Color(0, 153, 153));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 670, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 60, Short.MAX_VALUE)
+        );
+
+        TelaDocente.add(jPanel1);
+        jPanel1.setBounds(70, 90, 670, 60);
+
+        jPanel2.setBackground(new java.awt.Color(255, 204, 0));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 670, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 10, Short.MAX_VALUE)
+        );
+
+        TelaDocente.add(jPanel2);
+        jPanel2.setBounds(70, 150, 670, 10);
+
+        jLabel3.setText("       Turmas");
+
+        jLabel4.setText("       Cadastrar Aluno");
+
+        jLabel5.setText("        Docentes");
+
+        jLabel6.setText("        Disciplinas ");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 148, Short.MAX_VALUE))
+        );
+
+        TelaDocente.add(jPanel3);
+        jPanel3.setBounds(70, 160, 150, 350);
+        TelaDocente.add(jDesktopPane1);
+        jDesktopPane1.setBounds(220, 160, 520, 350);
+
+        BotaoSairDocente.setText("SAIR");
+        BotaoSairDocente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotaoSairDocenteActionPerformed(evt);
+            }
+        });
+        TelaDocente.add(BotaoSairDocente);
+        BotaoSairDocente.setBounds(700, 550, 72, 23);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagem/Point Blur_Nov142022_191653.jpg"))); // NOI18N
+        TelaDocente.add(jLabel2);
+        jLabel2.setBounds(0, 0, 800, 600);
+
+        TelaEstudante.setMinimumSize(new java.awt.Dimension(800, 600));
+        TelaEstudante.setOpaque(false);
+        TelaEstudante.setLayout(null);
+
+        jButton3.setText("Estudante");
+        TelaEstudante.add(jButton3);
+        jButton3.setBounds(340, 270, 130, 23);
+
+        jButton4.setText("SAIR");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        TelaEstudante.add(jButton4);
+        jButton4.setBounds(360, 320, 72, 23);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagem/Point Blur_Nov142022_191653.jpg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        jLabel1.setMaximumSize(new java.awt.Dimension(800, 600));
+        jLabel1.setMinimumSize(new java.awt.Dimension(800, 600));
+        jLabel1.setPreferredSize(new java.awt.Dimension(800, 600));
+        TelaEstudante.add(jLabel1);
+        jLabel1.setBounds(0, 0, 800, 600);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(TelaLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(TelaDocente, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(TelaEstudante, javax.swing.GroupLayout.PREFERRED_SIZE, 800, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(TelaLogin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(TelaDocente, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(TelaEstudante, javax.swing.GroupLayout.PREFERRED_SIZE, 600, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void ClickMouseUsuario(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ClickMouseUsuario
+      loginUsuario.setText(null);
+     loginUsuario.setFont(new java.awt.Font("Arial Unicode MS", 2, 12)); // NOI18N
+     loginUsuario.setForeground(new java.awt.Color(0, 0, 0));
+     
+    }//GEN-LAST:event_ClickMouseUsuario
+    
+    private void ImagemMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ImagemMouseClicked
+        if(loginUsuario.getText()== null){      
+       loginUsuario.setFont(new java.awt.Font("Gadugi", 2, 12)); // NOI18N
+        loginUsuario.setForeground(new java.awt.Color(204, 204, 204));
+        loginUsuario.setText("Usúario");
+        }
+        if(loginSenha.getText() == null){
+         loginSenha.setFont(new java.awt.Font("Gadugi", 2, 12)); // NOI18N
+        loginSenha.setForeground(new java.awt.Color(204, 204, 204));
+        loginSenha.setText("Senha");
+    }//GEN-LAST:event_ImagemMouseClicked
+    }
+    private void ClickMouseSenha(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ClickMouseSenha
+          loginSenha.setText(null);
+         loginSenha.setFont(new java.awt.Font("Arial Unicode MS", 2, 12)); // NOI18N
+     loginSenha.setForeground(new java.awt.Color(0, 0, 0));
+    }//GEN-LAST:event_ClickMouseSenha
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        String Username = loginUsuario.getText();
+        String Password = loginSenha.getText();    
+        
+        if(Username.contains("abc") && Password.contains("123")){//Procurar na lista de Estudantes/Aluno
+        loginUsuario.setText(null);
+        loginSenha.setText(null);
+        TelaLogin.setVisible(false);
+        TelaEstudante.setVisible(true);
+        //TelaDocente.setVisible(true);
+    }
+        if(Username.contains("def") && Password.contains("456")){//Procurar na lista de Professores
+        loginUsuario.setText(null);
+        loginSenha.setText(null);
+        TelaLogin.setVisible(false);
+        TelaDocente.setVisible(true);
+        //TelaEstudante.setVisible(false);
+    }//GEN-LAST:event_jButton1ActionPerformed
+    }
+    private void BotaoSairTelaLogin(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoSairTelaLogin
+      System.exit(0); 
+    }//GEN-LAST:event_BotaoSairTelaLogin
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        TelaEstudante.setVisible(false);
+        TelaLogin.setVisible(true);// TODO add your handling code here:
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void BotaoSairDocenteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotaoSairDocenteActionPerformed
+        TelaDocente.setVisible(false);
+        TelaLogin.setVisible(true);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BotaoSairDocenteActionPerformed
+
+    private void loginSenhaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_loginSenhaKeyPressed
+       getRootPane().setDefaultButton(jButton1); // TODO add your handling code here:
+    }//GEN-LAST:event_loginSenhaKeyPressed
+   
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Tela().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotaoSairDocente;
+    private javax.swing.JLabel Imagem;
+    private javax.swing.JPanel TelaDocente;
+    private javax.swing.JPanel TelaEstudante;
+    private javax.swing.JPanel TelaLogin;
+    private javax.swing.JLabel Título;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JTextField loginSenha;
+    private javax.swing.JTextField loginUsuario;
+    // End of variables declaration//GEN-END:variables
+
+    private void ClickMouseUsuario1() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
